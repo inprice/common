09785dcd437371f7db65b89b5c87e1cbcde01eaa
mdpinar <mdumlupinar@gmail.com>
2020-12-09 16:45:21 +0300

    step 6

--- src/main/java/io/inprice/common/meta/SubsEvent.java
+++ src/main/java/io/inprice/common/meta/SubsEvent.java
@@ -5,11 +5,19 @@ import java.util.Map;
 
 public enum SubsEvent {
 
-  FREE_USE("Free use started"),
+  FREE_USE_STARTED("Free use started"),
+  FREE_USE_STOPPED("Free use stopped"),
+  FREE_USE_CANCELLED("Free use cancelled"),
+
+  COUPON_USE_STARTED("Coupon use started"),
+  COUPON_USE_STOPPED("Coupon use stopped"),
+  COUPON_USE_CANCELLED("Coupon use cancelled"),
+
   SUBSCRIPTION_STARTED("Subscription started"),
-  SUBSCRIPTION_RENEWAL("Subscription renewed"),
+  SUBSCRIPTION_RENEWED("Subscription renewed"),
+  SUBSCRIPTION_STOPPED("Subscription stopped"),
   SUBSCRIPTION_CANCELLED("Subscription cancelled"),
-  COUPON_USED("Coupon used"),
+
   PAYMENT_FAILED("Payment failed");
 
   private String event;
