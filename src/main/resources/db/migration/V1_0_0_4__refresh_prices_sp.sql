9acb3f09ed7b8fe613a93de6264c43b8b90d2f40
mdpinar <mdumlupinar@gmail.com>
2021-11-15 05:15:23 +0300

    refresh_prices_sp.sql edited

--- src/main/resources/db/migration/V1_0_0_4__refresh_prices_sp.sql
+++ src/main/resources/db/migration/V1_0_0_4__refresh_prices_sp.sql
@@ -1,119 +1,99 @@
--- @author mdpinar
--- @since 2021-03-13
+-- _author mdpinar
+-- _since 2021-03-13
 DELIMITER $$
 
 create procedure sp_refresh_product (
     in in_product_id bigint,
-    out productPrice decimal(9,2),
-    out basePrice decimal(9,2),
-    out minPrice decimal(9,2),
-    out avgPrice decimal(9,2),
-    out maxPrice decimal(9,2),
+    out productPrice decimal(10,2),
+    out basePrice decimal(10,2),
+    out minPrice decimal(10,2),
+    out avgPrice decimal(10,2),
+    out maxPrice decimal(10,2),
     out position varchar(10),
     out alarmId bigint unsigned,
     out smartPriceId bigint unsigned,
     out actives int
   )
 begin
-  select price, base_price, alarm_id, smart_price_id into @productPrice, @basePrice, @alarmId, @smartPriceId from product where id = in_product_id for update;
+  declare _productPrice decimal(10,2) default 0;
+  declare _basePrice decimal(10,2) default 0;
+  declare _alarmId bigint unsigned;
+  declare _smartPriceId bigint unsigned;
 
-  select
-    @minPrice := min(case when grup = 'ACTIVE' then price end),
-    @avgPrice := avg(case when grup = 'ACTIVE' then price end),
-    @maxPrice := max(case when grup = 'ACTIVE' then price end),
-    @actives  := sum(case when grup = 'ACTIVE' then 1 else 0 end),
-    @tryings  := sum(case when grup = 'TRYING' then 1 else 0 end),
-    @waitings := sum(case when grup = 'WAITING' then 1 else 0 end),
-    @problems := sum(case when grup = 'PROBLEM' then 1 else 0 end)
-  from link where product_id = in_product_id;
+  declare _position varchar(7) default 'NotSet';
+  declare _minPrice decimal(10,2) default 0;
+  declare _minSeller varchar(50) default 'NA';
+  declare _minPlatform varchar(50) default 'NA';
+  declare _avgPrice decimal(10,2) default 0;
+  declare _maxPrice decimal(10,2) default 0;
+  declare _maxSeller varchar(50) default 'NA';
+  declare _maxPlatform varchar(50) default 'NA';
+  
+  declare _actives smallint default 0;
+  declare _tryings smallint default 0;
+  declare _waitings smallint default 0;
+  declare _problems smallint default 0;
 
-  if @minPrice is null then set @minPrice := 0; end if;
-  if @avgPrice is null then set @avgPrice := 0; end if;
-  if @maxPrice is null then set @maxPrice := 0; end if;
-  if @actives  is null then set @actives  := 0; end if;
-  if @tryings  is null then set @tryings  := 0; end if;
-  if @waitings is null then set @waitings := 0; end if;
-  if @problems is null then set @problems := 0; end if;
+  select price, base_price, alarm_id, smart_price_id into _productPrice, _basePrice, _alarmId, _smartPriceId from product where id = in_product_id for update;
 
-  set @position    := 'NotSet';
-  set @minSeller   := 'NA';
-  set @minPlatform := 'NA';
-  set @maxSeller   := 'NA';
-  set @maxPlatform := 'NA';
+  select sum(if(grup = 'ACTIVE', 1, 0)), sum(if(grup = 'TRYING', 1, 0)), sum(if(grup = 'WAITING', 1, 0)), sum(if(grup = 'PROBLEM', 1, 0))
+  into _actives, _tryings, _waitings, _problems from link where product_id = in_product_id;
 
-  set @minDiff := 0;
-  set @avgDiff := 0;
-  set @maxDiff := 0;
-
-  if @actives > 0 or @tryings > 0 or @waitings > 0 or @problems > 0 then
-
+  if _actives > 0 or _tryings > 0 or _waitings > 0 or _problems > 0 then
     update link set position='NotSet' where product_id=in_product_id;
 
-    if @actives > 1 then
+    if _actives > 1 then
+      select ifnull(min(price),0), ifnull(avg(price),0), ifnull(max(price),0) into _minPrice, _avgPrice, _maxPrice from link where product_id = in_product_id and grup = 'ACTIVE';
 
-      if @minPrice != @maxPrice then
-        select @minSeller := seller, @minPlatform:= p.name from link as l inner join platform as p on p.id = l.platform_id where price = @minPrice;
-        select @maxSeller := seller, @maxPlatform:= p.name from link as l inner join platform as p on p.id = l.platform_id where price = @maxPrice;
-      else
-        set @position := 'Equal';
-        set @avgPrice := @minPrice;
-      end if;
+      if _minPrice != _maxPrice then
+        select seller, p.name into _minSeller, _minPlatform from link as l inner join platform as p on p.id = l.platform_id where price = _minPrice;
+        select seller, p.name into _maxSeller, _maxPlatform from link as l inner join platform as p on p.id = l.platform_id where price = _maxPrice;
 
-      if @minPrice != @maxPrice then
-
-        if @minPrice != 0 then set @minDiff := @minPrice - @productPrice; end if;
-        if @avgPrice != 0 then set @avgDiff := @avgPrice - @productPrice; end if;
-        if @maxPrice != 0 then set @maxDiff := @maxPrice - @productPrice; end if;
-
-        if @productPrice <= @minPrice then 
-          set @position := 'Lowest';
-          set @minSeller := 'You';
-          set @minPlatform := 'Yours';
-          set @minPrice := @productPrice;
-          set @minDiff := 0;
-        elseif @productPrice < @avgPrice then 
-          set @position := 'Lower';
-        elseif @productPrice = @avgPrice then 
-          set @position := 'Average';
-          set @avgDiff := 0;
-        elseif @productPrice < @maxPrice then 
-          set @position := 'Higher';
-        elseif @productPrice >= @maxPrice then 
-          set @position := 'Highest';
-          set @maxSeller := 'You';
-          set @maxPlatform := 'Yours';
-          set @maxPrice := @productPrice;
-          set @maxDiff := 0;
+        if _productPrice <= _minPrice then 
+          set _position := 'Lowest';
+          set _minSeller := 'You';
+          set _minPlatform := 'Yours';
+          set _minPrice := _productPrice;
+        elseif _productPrice < _avgPrice then 
+          set _position := 'Lower';
+        elseif _productPrice = _avgPrice then 
+          set _position := 'Average';
+        elseif _productPrice < _maxPrice then 
+          set _position := 'Higher';
+        elseif _productPrice >= _maxPrice then 
+          set _position := 'Highest';
+          set _maxSeller := 'You';
+          set _maxPlatform := 'Yours';
+          set _maxPrice := _productPrice;
         end if;
-      end if;
 
-      if @minPrice != @maxPrice then
         update link set position = 
         (case
-            when price <= @minPrice then 'Lowest'
-            when price > @minPrice and price < @avgPrice then 'Lower'
-            when price = @avgPrice then 'Average'
-            when price > @avgPrice and price < @maxPrice then 'Higher'
-            when price >= @maxPrice then 'Highest'
+            when price <= _minPrice then 'Lowest'
+            when price > _minPrice and price < _avgPrice then 'Lower'
+            when price = _avgPrice then 'Average'
+            when price > _avgPrice and price < _maxPrice then 'Higher'
+            when price >= _maxPrice then 'Highest'
           end)
         where product_id=in_product_id and grup='ACTIVE';
       else
+        set _position := 'Equal';
+        set _avgPrice := _minPrice;
         update link set position = 'Equal' where product_id=in_product_id and grup='ACTIVE';
       end if;
 
     end if;
-
   end if;
 
   update product set
-    min_seller = @minSeller, min_platform = @minPlatform, min_price = @minPrice, min_diff = @minDiff,
-    avg_price = @avgPrice, avg_diff = @avgDiff,
-    max_seller = @maxSeller, max_platform = @maxPlatform, max_price = @maxPrice, max_diff = @maxDiff,
-    actives = @actives, tryings = @tryings, waitings = @waitings, problems = @problems, 
-    position = @position, updated_at = now()
+    min_seller = _minSeller, min_platform = _minPlatform, min_price = _minPrice, min_diff = (_minPrice - _productPrice),
+    avg_price = _avgPrice, avg_diff = (_avgPrice - _productPrice),
+    max_seller = _maxSeller, max_platform = _maxPlatform, max_price = _maxPrice, max_diff = (_maxPrice - _productPrice),
+    actives = _actives, tryings = _tryings, waitings = _waitings, problems = _problems, 
+    position = _position, updated_at = now()
   where id = in_product_id;
 
-  select @productPrice, @basePrice, @minPrice, @avgPrice, @maxPrice, @position, @alarmId, @smartPriceId, @actives 
+  select _productPrice, _basePrice, _minPrice, _avgPrice, _maxPrice, _position, _alarmId, _smartPriceId, _actives 
   into productPrice, basePrice, minPrice, avgPrice, maxPrice, position, alarmId, smartPriceId, actives;
 
 end$$
